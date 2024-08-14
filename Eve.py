658f2b904891a93cbc518e35e94befca06f72079
Evelyn Paloma <eve.paloma92e@mail.com>
2024-08-14 19:28:14 -0300

    Hice funcion binomial e hipergeometrica

--- Eve.py
+++ Eve.py
@@ -17,4 +17,24 @@ def combinatorio(n,k):
     #calculamos num comb usando formula: n! / k! * (n - k)!
     return factorial(n) // (factorial(k) * factorial(n - k))
     #usamos la funcion factorial para calcular los factoriales necesarios.
-    
+
+#funcion para calcular la probabilidad binomial
+def probabilidad_binomial(n,p,k):
+    #calculamos la probabilidad combinatoria multiplicada por la probabilidad p elevada a la k
+    #y la probabilidad de fracaso (1 - p) elevada a n - k
+    comb = combinatorio(n,k)    
+    prob = comb * (p ** k) * ((1 - p) ** (n - k))
+    #se redondea la prob a 4 decimales
+    return round(prob,4)
+
+#funcion para calcular probabilidad hipergeometrica
+def probabilidad_hipergeometrica(N,K,n,k):
+    #calculamos C(K,k) que es el num de formas de elegir k éxitos de k éxitos en la poblacion
+     comb1 = combinatorio(K,k)
+     #calculamos C(N-K, n -k) que es el num de formas de elegir los fracasos de los no-exitos en la poblacion
+     comb2 = combinatorio(N - K, n - k)
+     #calculamos C(N,n) que es el num de formas de elegir n elementos de una poblacion de tamaño N
+     comb3 = combinatorio(N,n)
+     #calculamos la probabilidad usando la formula de la probabilidad hipergeometrica
+     prob = (comb1 * comb2) / comb3
+     return round(prob, 4)
