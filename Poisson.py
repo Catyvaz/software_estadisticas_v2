1f0a79f302f89a3363fe3bd7f1b941f4d25adef2
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-08-14 21:13:07 -0300

    comienzo código de poisson

---
+++ Poisson.py
@@ -0,0 +1,14 @@
+from math import e
+
+def Poisson(lamda, x)
+    lamda = cantidad    
+    resultado = (e ** (-lamda)) * (lamda ** espera)
+    
+    return
+
+print("Poisson, número de ocurrencias de un evento en un intervalo")
+
+cantidad = int(input("Lo que se espera: "))
+intervalo = int(input("Intervalo: "))
+usuario_espera = int(input("¿Qué probabilidad quiere calcular?: "))
+usuario_intervalo = float(input("¿En que intervalo?: "))
