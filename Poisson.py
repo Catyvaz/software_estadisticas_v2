fc55543cac4f3ee7e499bc4b0fff14204b3be42c
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-08-14 22:06:19 -0300

    Poisson terminado para una sola probabilidad o varias

--- Poisson.py
+++ Poisson.py
@@ -1,14 +1,45 @@
 from math import e
 
-def Poisson(lamda, x)
-    lamda = cantidad    
-    resultado = (e ** (-lamda)) * (lamda ** espera)
+"""
+def factorial(n):
+    #si n es 0 o 1, el factorial es 1
+    if n == 0 or n == 1:
+        return 1
+    #inicia el resultado en 1
+    resultado = 1
+    #bucle para multiplicar todos los num desde 2 hasta n
+    for i in range(2, n + 1):
+        #multiplica el resultado por i en cada interacion.
+        resultado *= i 
+     #retorna el factorial calculado    
+    return resultado
+"""
+def factorial(numero):
+    if numero == 0:
+        return 1
+    return numero * factorial(numero - 1)
+
+def Poisson(lamda, x):
+    lamda = cantidad
+    factorial_x = factorial(x)    
+    resultado = ((e ** (-lamda)) * (lamda ** x)) / factorial_x
     
-    return
+    return round(resultado, 6)
 
-print("Poisson, número de ocurrencias de un evento en un intervalo")
+while True:
+    print("")
+    print("Poisson, número de ocurrencias de un evento en un intervalo")
+    valor = int(input("Cuantos valores de probabilidad? \n Un solo valor = 1 \n Varios valores = 2 \n --> "))
+    if valor == 1:
+        cantidad = float(input("Lo que se espera: "))
+        usuario_espera = float(input("¿Qué probabilidad quiere calcular?: "))
 
-cantidad = int(input("Lo que se espera: "))
-intervalo = int(input("Intervalo: "))
-usuario_espera = int(input("¿Qué probabilidad quiere calcular?: "))
-usuario_intervalo = float(input("¿En que intervalo?: "))
+        print(f"El resultado es: ", Poisson(cantidad, usuario_espera))
+
+    elif valor == 2:
+        limite = int(input("x o más: "))
+        cantidad = float(input("Lo que se espera: "))
+        resultado = 0
+        for i in range(limite):
+            resultado += Poisson(cantidad, i)
+        print(f"El resultado es: ", round(1 - resultado, 6))
