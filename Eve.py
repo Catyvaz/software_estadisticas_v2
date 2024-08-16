bc3a32212d54238c94bcb4e7fb8346e4fabba4de
Evelyn Paloma <eve.paloma92e@mail.com>
2024-08-16 20:52:40 -0300

    Le agregue un menu con opciones por ser varias funciones para poder probar el código.

--- Eve.py
+++ Eve.py
@@ -46,3 +46,46 @@ def probabilidad_hipergeometrica(N,K,n,k):
      #num de exitos en la poblacion(K)
      #tamaño de muestra(n)
      #num de exitos en la muestra(k)
+
+
+def menu():
+    while True:
+        print("\nSeleccion de funcion a probar:")
+        print("1. Calcular Factorial")
+        print("2. Calcular Combinatorio")
+        print("3. Calcular Probabilidad Binomial")
+        print("4. Calcular Probabilidad Hipergeométrica")
+        print("5. Salir")
+
+        opcion = input("Ingrese una opción: ")
+
+        if opcion == "1":
+            n = int(input("Ingrese el valor de n: "))
+            print(f"Factorial de {n} es: {factorial(n)}")
+
+        elif opcion == "2":
+            n = int(input("Ingrese el valor de n: "))
+            k = int(input("Ingrese el valor de k: "))
+            print(f"Combinatorio C({n},{k} es: {combinatorio(n,k)})")
+
+        elif opcion == "3":
+            n = int(input("Ingrese el número de ensayos (n): "))
+            p = float(input("Ingrese la probabilidad de éxito (p): "))
+            k = int(input("Ingrese el número de éxitos deseados (k): "))
+            print(f"Probabilidad Binomial P(X = {k}) es: {probabilidad_binomial(n,p,k)}")
+
+        elif opcion == "4":
+            N = int(input("Ingrese el tamaño de la población (N): "))
+            K = int(input("Ingrese el número de éxitos en la población (K): "))
+            n = int(input("Ingrese el tamaño de la muestra (n): "))
+            k = int(input("Ingrese el número de éxitos en la muestra (k): "))
+            print(f"Probabilidad Hipergeométrica es: {probabilidad_hipergeometrica(N,K,n,k)}")
+
+        elif opcion == "5":
+            print("Creo que anda todo")
+            break
+
+        else:
+            print("Opcion no valida. Intente de nuevo.")
+
+menu()                            
