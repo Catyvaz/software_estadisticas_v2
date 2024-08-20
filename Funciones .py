e9f7b2f01c0a5d5f337a1f4064bbaf999e0807cc
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-08-20 18:45:10 -0300

    cambios curto

--- Funciones .py
+++ Funciones .py
@@ -141,27 +141,3 @@ while True:
         for i in range(limiteI, limiteF + 1):
             resultado += Poisson(cantidad, i)
         print(f"El resultado es: ", round(resultado, 5))
-
-def calcular_curtosis(datos):
-    n = len(datos)
-    media = sum(datos) / n
-    varianza = sum((x - media) ** 2 for x in datos) / n
-    desviacion_estandar = varianza ** 0.5
-
-    curtosis = sum((x - media) ** 4 for x in datos) / (n * desviacion_estandar ** 4)
-    
-    # Ajuste de Fisher para obtener la curtosis con sesgo corregido
-    curtosis_fisher = ((n * (n + 1)) / ((n - 1) * (n - 2) * (n - 3))) * curtosis - (3 * (n - 1) ** 2) / ((n - 2) * (n - 3))
-    
-    return curtosis_fisher
-    
-datos = [1, 2, 2, 3, 4, 5, 5, 6, 7, 8]
-curtosis = calcular_curtosis(datos)
-if curtosis > 0:
-    tipocurtosis= "Leptocúrtica"
-if curtosis == 0:
-    tipocurtosis = "Mezzocúrtica"
-if curtosis < 0:
-    tipocurtosis= "Platicúrtica"
-
-print(f"El coeficiente de curtosis es: {curtosis} y según su resultado es: " , tipocurtosis)
