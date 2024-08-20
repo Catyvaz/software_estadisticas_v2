a3d4d24160a1d95ed270c9e16362d89bec3ee844
Caty <123335198+Catyvaz@users.noreply.github.com>
2024-08-20 13:47:35 -0300

    casi

--- Poisson.py
+++ Poisson.py
@@ -13,11 +13,6 @@ def factorial(n):
      #retorna el factorial calculado    
     return resultado
 
-def validar_noneg(valor):
-    if valor < 0:
-        print("El valor debe ser mayor a 0.")
-        break
-
 def Poisson(lamda, x):
     lamda = cantidad
     factorial_x = factorial(x)    
@@ -29,19 +24,16 @@ while True:
     print("")
     print("Poisson, número de ocurrencias de un evento en un intervalo")
     print("Recuerde que la probabilidad de ocurrencia debe ser un numero entero mayor que 0")
+    cantidad = float(input("Promedio de ocurrencias en x intervalo: "))
     valor = int(input("Cuantas veces debe ocurrir el evento? \n Un caso = 1 \n Varios casos = 2 \n --> "))
     if valor == 1:
-        cantidad = float(input("Promedio de ocurrencias en x intervalo: "))
         usuario_espera = int(input("¿Qué probabilidad quiere calcular?\n x = "))
-        validar_noneg(cantidad)
-        validar_noneg(usuario_espera)
         print(f"El resultado es: ", Poisson(cantidad, usuario_espera))
-
     elif valor == 2:
         print("Ingrese la probabilidad de ocurrencia de cuales casos quiere calcular, separandolos con un espacio y luego precionando enter. \nejemplo: 1 2 3 4 (enter)")
-        limite = list(input("Ocurrencias \nx = "))
-        cantidad = float(input("Promedio de ocurrencias en x intervalo: "))
+        limiteI = int(input("Que ocurra desde \nx = "))
+        limiteF = int(input("Hasta \nx = "))
         resultado = 0
-        for i in range(limite):
+        for i in range(limiteI, limiteF + 1):
             resultado += Poisson(cantidad, i)
-        print(f"El resultado es: ", round(resultado, 6))
+        print(f"El resultado es: ", round(resultado, 5))
