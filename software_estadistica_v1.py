81fc7e4f42ef9a8ae8376c4a69b89edb871b6e6b
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-08-19 23:42:58 -0300

    Update software_estadistica_v1.py
    se le agregó el cálculo del coeficiente de curtosis y su opción en el menú

--- software_estadistica_v1.py
+++ software_estadistica_v1.py
@@ -97,6 +97,29 @@ def frecuencias(lista_ordenada: dict) -> list:
 
     return filas # Retorna la lista con las líneas de la tabla
 
+def calcular_curtosis(lista: List[float]) -> Tuple[float, str]:
+    n = len(lista)
+    if n < 4:
+        return None, "No se puede calcular la curtosis con menos de 4 datos"
+
+    res_media = media(lista)
+    res_desviacion_estandar = desviacion_estandar(lista)
+
+    suma_curtosis = sum((numero - res_media) ** 4 for numero in lista)
+    curtosis = suma_curtosis / (n * res_desviacion_estandar ** 4)
+    
+    # Ajuste de Fisher para obtener la curtosis con sesgo corregido
+    curtosis_fisher = ((n * (n + 1)) / ((n - 1) * (n - 2) * (n - 3))) * curtosis - (3 * (n - 1) ** 2) / ((n - 2) * (n - 3))
+
+    if curtosis_fisher > 0:
+        tipo_curtosis = "Leptocúrtica"
+    elif curtosis_fisher == 0:
+        tipo_curtosis = "Mesocúrtica"
+    else:
+        tipo_curtosis = "Platicúrtica"
+    
+    return curtosis_fisher, tipo_curtosis  # Retorna el coeficiente de Curtosis y su tipo
+
 # Funciones de validación de entrada
 
 def input_int(prompt: str) -> int: #013: Función que verifica que se ingrese un entero en la sección del menú
@@ -256,3 +279,13 @@ Opción elegida: """)
 
     else:
         print("Opción no válida, por favor seleccione una opción válida.")
+
+    if opcion_a == 4:
+        res_curtosis, tipo_curtosis = calcular_curtosis(nros)
+        if res_curtosis is not None:
+            print(f"El coeficiente de curtosis es: {res_curtosis:.4f} y según su resultadoto es: {tipo_curtosis}")
+        else:
+            print("No se puede calcular la curtosis con menos de 4 datos.")
+    else:
+        print("Opción no válida, por favor seleccione una opción válida.")
+    
