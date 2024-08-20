61f357b6330ca35e7f14cbf12a509d0dbe4cffc6
Sofía Valles <168451567+SofiaValles@users.noreply.github.com>
2024-08-20 18:49:48 -0300

    Update curtosis.py

--- curtosis.py
+++ curtosis.py
@@ -1,24 +1,37 @@
-def calcular_curtosis(datos):
-    n = len(datos)
-    media = sum(datos) / n
-    varianza = sum((x - media) ** 2 for x in datos) / n
-    desviacion_estandar = varianza ** 0.5
+def calcular_curtosis(lista: List[float]) -> Tuple[float, str]:
+    n = len(lista)
+    if n < 4:
+        return None, "No se puede calcular la curtosis con menos de 4 datos"
 
-    curtosis = sum((x - media) ** 4 for x in datos) / (n * desviacion_estandar ** 4)
+    res_media = media(lista)
+    res_desviacion_estandar = desviacion_estandar(lista)
+
+    suma_curtosis = sum((numero - res_media) ** 4 for numero in lista)
+    curtosis = suma_curtosis / (n * res_desviacion_estandar ** 4)
     
     # Ajuste de Fisher para obtener la curtosis con sesgo corregido
     curtosis_fisher = ((n * (n + 1)) / ((n - 1) * (n - 2) * (n - 3))) * curtosis - (3 * (n - 1) ** 2) / ((n - 2) * (n - 3))
+
+    if curtosis_fisher > 0:
+        tipo_curtosis = "Leptocúrtica"
+    elif curtosis_fisher == 0:
+        tipo_curtosis = "Mesocúrtica"
+    else:
+        tipo_curtosis = "Platicúrtica"
     
-    return curtosis_fisher
+    return curtosis_fisher, tipo_curtosis  # Retorna el coeficiente de Curtosis y su tipo
 
-# Ejemplo de uso
-datos = [1, 2, 2, 3, 4, 5, 5, 6, 7, 8]
-curtosis = calcular_curtosis(datos)
-if curtosis > 0:
-    tipocurtosis= "Leptocúrtica"
-if curtosis == 0:
-    tipocurtosis = "Mezzocúrtica"
-if curtosis < 0:
-    tipocurtosis= "Platicúrtica"
+"""0 = Salir
+    1 = Medidas de posición         
+    2 = Medidas de dispersión
+    3 = Tabla de frecuencias  
+    4 = Curtosis"""
 
-print(f"El coeficiente de curtosis es: {curtosis} y según su resultado es: " , tipocurtosis)
+if opcion_a == 4:
+        res_curtosis, tipo_curtosis = calcular_curtosis(nros)
+        if res_curtosis is not None:
+            print(f"El coeficiente de curtosis es: {res_curtosis:.4f} y según su resultadoto es: {tipo_curtosis}")
+        else:
+            print("No se puede calcular la curtosis con menos de 4 datos.")
+    else:
+        print("Opción no válida, por favor seleccione una opción válida.")
