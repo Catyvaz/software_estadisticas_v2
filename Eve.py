a2c6420e35a8063d5ac8c79452973a821d4de8db
Evelyn Paloma <eve.paloma92e@mail.com>
2024-08-25 22:21:26 -0300

    Borrador

--- Eve.py
+++ Eve.py
@@ -1,3 +1,85 @@
+#CODIGO QUE BORRE DE FUNCIONES DE CATY CAMBIOS PARA PONER EL NUEVO:
+'''
+#Funcion para calcular el factorial de un num n
+def factorial(n):
+    #validamos datos dentro del bucle
+    while n < 0:
+        print("Error: n debe ser un numero entero positivo")
+        n = int(input("Ingrese un número para calcular el factorial"))
+    #si n es 0 o 1, el factorial es 1
+    if n == 0 or n == 1:
+        return 1
+    #inicia el resultado en 1
+    resultado = 1
+    #bucle para multiplicar todos los num desde 2 hasta n
+    for i in range(2, n + 1):
+        #multiplica el resultado por i en cada interacion.
+        resultado *= i 
+     #retorna el factorial calculado    
+    return resultado
+
+#funcion para calcular el num comb.
+def combinatorio(n,k):
+    #validamos que k(éxitos de muestra) no sea mayor que n(tamaño de la muestra)
+    while k > n:
+        print("Error. k no puede ser mayor que n")
+        k = int(input(f"Ingrese un valor para k (menor o igual a {n}): "))
+    #calculamos num comb usando formula: n! / k! * (n - k)!
+    return factorial(n) // (factorial(k) * factorial(n - k))
+    #usamos la funcion factorial para calcular los factoriales necesarios.
+
+#funcion para calcular la probabilidad binomial
+def probabilidad_binomial(n,p,k):
+    #Validacion de datos
+    while p < 0 or p > 1:
+        print("Error. La probabilidad p debe estar entre 0 y 1")
+        p = float(input("Ingrese la probabilidad de éxito p (entre 0 y 1): "))
+
+    #Validamos que el núm de éxitos k no sea mayor al núm de ensayos n. 
+    while k > n:
+        print("Error: El número de éxitos k no puede ser mayor que el número de ensayos n.")
+        k = int(input(f"Ingrese un valor para k (menor o igual a {n}): "))
+
+    #calculamos la probabilidad combinatoria multiplicada por la probabilidad p elevada a la k
+    #y la probabilidad de fracaso (1 - p) elevada a n - k
+    comb = combinatorio(n,k)    
+    prob = comb * (p ** k) * ((1 - p) ** (n - k))
+    #se redondea la prob a 4 decimales
+    return round(prob,4)
+    #num de ensayos(n)
+    #probabilidad de exitos(p)
+    #num de exitos(k)
+
+
+#funcion para calcular probabilidad hipergeometrica
+def probabilidad_hipergeometrica(N,K,n,k):
+     while K > N:
+         print("Error: K(éxitos en la poblacion) no puede ser mayor que N(tamaño de la población).")
+         K = int(input(f"Ingrese un valor para K (menor o igual  a {N}): "))
+     while k > n:
+         print("Error: k(éxitos en la muestra) no puede ser mayor que n(tamaño de muestra).")
+         k = int(input(f"Ingrese un valor para k (menor o igual a {n}): "))
+     while k > K:
+         print("Error: k(éxitos en la muestra) no puede ser mayor que K(éxitos en la poblacion).")
+         k = int(input(f"Ingrese un valor para k (menor o igual a {K}): "))        
+    #calculamos C(K,k) que es el num de formas de elegir k éxitos de k éxitos en la poblacion     
+     comb1 = combinatorio(K,k)
+     #calculamos C(N-K, n -k) que es el num de formas de elegir los fracasos de los no-exitos en la poblacion
+     comb2 = combinatorio(N - K, n - k)
+     #calculamos C(N,n) que es el num de formas de elegir n elementos de una poblacion de tamaño N
+     comb3 = combinatorio(N,n)
+
+     while comb3 == 0:
+         print("Error: Division por cero. Verifica los valores de N y n.")
+         N = int(input("Ingrese el tamaño de la población N: "))
+     #calculamos la probabilidad usando la formula de la probabilidad hipergeometrica
+     prob = (comb1 * comb2) / comb3
+     return round(prob, 4)
+     #tamaño de la poblacion(N)
+     #num de exitos en la poblacion(K)
+     #tamaño de muestra(n)
+     #num de exitos en la muestra(k)
+'''
 '''
 #Funcion para calcular el factorial de un num n
 def factorial(n):
@@ -123,7 +205,7 @@ def menu():
 menu()   
 
                          '''
-
+'''
 def factorial(n):
     #validamos datos dentro del bucle
     while n < 0:
@@ -230,4 +312,4 @@ N = val_numeros("Ingrese el tamaño de la población (N): ")
 K = val_numeros("Ingrese el número de éxitos en la población (K): ")
 n = val_numeros("Ingrese el tamaño de la muestra (n): ")
 k = val_numeros("Ingrese el número de éxitos en la muestra (k): ")
-print(f"Probabilidad Hipergeométrica es: {probabilidad_hipergeometrica(N, K, n, k)}")
+print(f"Probabilidad Hipergeométrica es: {probabilidad_hipergeometrica(N, K, n, k)}")'''
